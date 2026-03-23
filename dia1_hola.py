a21e01115a6f7406c903da118abc0a654adf7221
Samuel Mendoza <mendozamerazoscarsamuel@gmail.com>
2026-03-23 08:55:50 -0600

    Dia 1 terminado

--- dia1_hola.py
+++ dia1_hola.py
@@ -1,5 +1,14 @@
-nombre = input("como te llamas? ")
+nombre = input("Como te llamas? ")
+edad = input("Cuantos años tienes: ")
+ciudad = input("En que ciudad vives: ")
 
-print("hola", nombre)
+edad = int(edad)
+nacimiento = 2026 - edad
+
+print(f"hola, {nombre}")
 print("Bienvenido al mundo de la programacion")
 print("Este es tu primer programa")
+print(f"Te llamas {nombre}")
+print(f"Naciste en el año {nacimiento}")
+print(f"Tienes {edad}")
+print(f"Vives en la {ciudad}")
